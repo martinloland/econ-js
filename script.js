a30da5a5433c6f2fdf81253b613a1cba20594617
Martin Loeland <martinloland@gmail.com>
2018-06-18 22:33:51 +0200

    working simple

---
+++ script.js
@@ -0,0 +1,37 @@
+Vue.component('phrase', {
+    template: '\
+    <li>\
+      {{ title }}\
+      <button v-on:click="$emit(\'remove\')">Remove</button>\
+    </li>\
+  ',
+    props: ['title']
+})
+
+new Vue({
+    el: '#todo-list-example',
+    data: {
+        newTodoText: '',
+        todos: [
+            {
+                id: 1,
+                title: 'rema 1000',
+            },
+            {
+                id: 2,
+                title: 'bunnpris',
+            }
+        ],
+        nextTodoId: 3,
+        cat_title: 'Mat'
+    },
+    methods: {
+        addNewTodo: function () {
+            this.todos.push({
+                id: this.nextTodoId++,
+                title: this.newTodoText
+            })
+            this.newTodoText = ''
+        }
+    }
+})
